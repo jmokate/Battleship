9a8e23d3ac8a09f3042e5ca2f778d408d263916e
jmokate <jmokate@gmail.com>
2021-07-21 12:30:16 -0500

    Check for valid input

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Battleship
 {
@@ -8,6 +9,10 @@ namespace Battleship
         {
             Game game = new Game();
             game.SetShipCoordinates();
+            List<char> letters = new List<char>()
+            {
+                'A','B','C','D','E','F','G','H','I','J'
+            };
             Console.WriteLine("Welcome to Battleship!\nHere are the Rules: ");
             Console.WriteLine("The game is played on a 10 x 10 grid, vertically from 'A' to 'J' and horizontally from '0' to '10'.\nAn example guess would be 'C8'.");
             Console.WriteLine("Try to sink the ship by guessing from 'A0' to 'J10'. You have 8 shots to fire, let's start!");
@@ -26,12 +31,15 @@ namespace Battleship
 
                 string guess = Console.ReadLine();
                 guess = char.ToUpper(guess[0]) + guess.Substring(1);
-                if (string.IsNullOrEmpty(guess))
+                if (string.IsNullOrEmpty(guess) || !letters.Contains(guess[0]))
                 {
-                    Console.WriteLine("enter between A0 and J10");
+                    Console.WriteLine("Please enter between A0 and J10");
+                    
                 }
-                
-                game.CheckGuess(guess);
+                else
+                {
+                    game.CheckGuess(guess);
+                }
             }
         }
     }
